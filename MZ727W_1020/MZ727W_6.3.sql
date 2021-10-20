22ac018f05dbc5626df6801d9ef94472979effc2
Simonyak Janos <simonyak@lab104-15.iit.uni-miskolc.hu>
2021-10-20 13:35:41 +0200

    6. gyak 3. feladat

---
+++ MZ727W_1020/MZ727W_6.3.sql
@@ -0,0 +1,20 @@
+CREATE TABLE Tanfolyam (
+    tkod int PRIMARY KEY,
+    ar int,
+    tipus char,
+    negnevezes char
+);
+
+CREATE TABLE Resztvevo (
+    tajszam int PRIMARY KEY,
+    nev char,
+    lakcim char
+);
+
+CREATE TABLE Tan_Resz (
+    tkod int,
+    tajszam int,
+    befizetes int,
+    FOREIGN KEY (tkod) REFERENCES Tanfolyam(tkod)
+    FOREIGN KEY (tajszam) REFERENCES Resztvevo(tajszam)
+)
